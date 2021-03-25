91cde8db9934bcf789f98200cd21f16cfcefd415
kavitakul <kavikulkarni25@gmail.com>
2021-03-25 12:43:01 +0530

    third commit

--- first-maven-app/src/main/java/com/lti/view/EmpView.java
+++ first-maven-app/src/main/java/com/lti/view/EmpView.java
@@ -6,8 +6,8 @@ public class EmpView {
 
 	public static void main(String[] args) {
 		Employee emp=new Employee();
-		emp.setEmpId(1002);
-		emp.setEmpName("mark");
+		emp.setEmpId(1003);
+		emp.setEmpName("kavita");
 		System.out.println("emp id:"+emp.getEmpId());
 		System.out.println("emp name:"+emp.getEmpName());
 		
